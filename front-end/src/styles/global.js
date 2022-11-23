e25be626f820ca19afd1a2cd5b8051c14cecf158
Micael Santos <micaelmaicon@hotmail.com>
2022-11-23 11:11:26 -0300

    feat: create routes register and *

--- front-end/src/styles/global.js
+++ front-end/src/styles/global.js
@@ -1,16 +1,19 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+
   * {
+    box-sizing: border-box;
     margin: 0;
     padding: 0;
-    box-sizing: border-box;
   }
 
   body {
     background-color: #f0f2f5;
     font-family: Arial, Helvetica, sans-serif;
     height: 100vh;
-    width: 100vw
+    width: 100vw;
   }
-  `;
+`;
+
+export default GlobalStyle;
