1394dc908e720e246c2c4a7324bd59d487b98540
Micael Santos <micaelmaicon@hotmail.com>
2022-11-28 14:54:32 -0300

    Revert "feat: create provider"
    This reverts commit dfdc111aa829668c0c5565a0af452705024a2fff.

--- front-end/src/pages/Home/index.js
+++ front-end/src/pages/Home/index.js
@@ -1,23 +1,8 @@
-import React, { useContext } from 'react';
-import { AuthContext } from '../../contexts/AuthContext';
-import { Section } from './styles';
-import { Header } from '../../components/Header'
-import { Resume } from '../../components/Resume';
-import { Transaction } from '../../components/Transaction';
+import React from "react";
 
 const Home = () => {
-  const auth = useContext(AuthContext) // utilizar userLogin()
-  const handleLogout = () => auth.userLogout();
-
   return (
-    <>
-      <Section>
-        <Header />
-        <Resume />
-        <Transaction data={auth.transactions}/>
-        {auth.user && <button name="btn_signout" onClick={handleLogout}>Sair</button>}
-      </Section>
-    </>
+    <div>Home</div>
   )
 }
 
--- front-end/src/pages/Login/index.js
+++ front-end/src/pages/Login/index.js
@@ -1,72 +1,9 @@
-import React, { useContext, useEffect, useState } from "react";
-import { AuthContext } from "../../contexts/AuthContext";
-// import * as C from './styles';
-// import { Button } from "../../components/Button";
+import React from "react";
 
-export const Login = () => {
-
-  const auth = useContext(AuthContext); // utilizar userLogin()
-
-  console.log('Cheguei no Login');
-
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [hidden, setHidden] = useState(true);
-  const [disabled, setDisabled] = useState(true);
-
-  const handleChange = ({ target }) => {
-    const { name, value } = target;
-    name === 'username' ? setUsername(value) : setPassword(value);
-  };
-
-  const handleLogin = async () => {
-    try {
-      if (username && password) {
-        const isLogged = await auth.userLogin(username, password);
-        console.log(isLogged);
-      }
-    } catch {
-      return setHidden(false);
-    }
-  };
-
-  useEffect(() => {
-    const minLengthUsername = 3;
-    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/;
-
-    if (passwordRegex.test(password)
-      && username.length >= minLengthUsername
-    ) {
-      setDisabled(false);
-    } else {
-      setDisabled(true);
-    }
-  }, [username, password]);
-
-  return (
-    <section className="main-login">
-      <h2>Entrar</h2>
-      <input
-        name="username"
-        onChange={handleChange}
-        placeholder="Usuário"
-        type="text"
-        value={username}
-      />
-      <input
-        name="password"
-        onChange={handleChange}
-        placeholder="Senha"
-        type="password"
-        value={password}
-      />
-      <p hidden={hidden}>Se o usuário existir, as credenciais estão incorrretas!</p>
-      <button
-        disabled={disabled}
-        onClick={handleLogin}>
-        Login
-      </button>
-
-    </section>
-  );
+const Login = (props) => {
+  const { name } = props;
+  return (<h1>No login {name}</h1>
+  )
 }
+
+export default Login
